93f5d1f9b90e254864f6865597e87b7199573c35
lahjal-sys <sopowaelahjal@gmail.com>
2026-02-27 17:18:57 +0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['image.pollinations.ai'],
+  },
+}
+
+module.exports = nextConfig
